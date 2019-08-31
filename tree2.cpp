8308c2d9d2ac6825343d1e22ed0bc0166c309cf1
khanmosajjid <mosajjid.khan@gmail.com>
2019-08-31 13:45:02 +0530

    finding height of a tree

--- tree2.cpp
+++ tree2.cpp
@@ -42,11 +42,32 @@ void printInorder(node *root){
 	printInorder(root->right);
    
 }
+  void levelOrder(node *root){
+  	queue<struct node*> q;
+  	if(root==NULL){
+  		return;
+  	}
+  	q.push(root);
+  	while(!q.empty()){
+  		struct node *temp=q.front();
+  		cout<<temp->data<<" ";
+  		q.pop();
+
+  		if(temp->left!=NULL)
+  			q.push(temp->left);
+  		if(temp->right!=NULL)
+  			q.push(temp->right);
+  		
+  	}
+  }
 
 int main(){
 	root=buildTree();
 	cout<<" post order traversal is"<<endl;
 	printPostorder(root);
-	cout<<" in oredr traversal is"<<ednl;
+	cout<<" in oredr traversal is"<<endl;
 	printInorder(root);
+	cout<<""<<endl;
+	// cout<<"leve order is"<<levelOrder(root)<<endl;
+	levelOrder(root);
 }
